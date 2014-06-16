ed5f20bb46ccd9571d1abb965ebc2792e1d0e5db
Kevin Breslin <kevin.breslin@icloud.com>
2014-06-16 01:14:37 -0400

    Clean up main class and show JSON and XML output

--- src/main/java/com/codeski/nbt/Main.java
+++ src/main/java/com/codeski/nbt/Main.java
@@ -4,13 +4,21 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import com.codeski.nbt.tags.NBTCompound;
+
 public class Main
 {
 	public static void main(String[] args) throws FileNotFoundException, IOException
 	{
 		File f = new File(args[0]);
 		System.out.println("Start reading " + f.getAbsolutePath() + "...");
-		System.out.println(new NBTReader(f).read());
+		NBTCompound root = (NBTCompound) new NBTReader(f).read();
 		System.out.println("Finished reading " + f.getAbsolutePath() + "...");
+		System.out.println();
+		System.out.println("{ " + root.toJSON() + " }");
+		System.out.println();
+		System.out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + root.toXML());
+		System.out.println();
+		System.out.println("Done!");
 	}
 }
