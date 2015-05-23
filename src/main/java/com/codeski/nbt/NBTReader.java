1562ff6a4c8a3b0be7799b1f1e6f8eed95a9b881
Kevin Breslin <kevin.breslin@icloud.com>
2015-05-23 02:07:51 -0400

    Consolidate reading of payload into one method

--- src/main/java/com/codeski/nbt/NBTReader.java
+++ src/main/java/com/codeski/nbt/NBTReader.java
@@ -62,20 +62,20 @@ public class NBTReader {
 		return magic == GZIPInputStream.GZIP_MAGIC;
 	}
 
-	private NBT<?> readPayload(final byte type) throws IOException {
+	private NBT<?> readPayload(final byte type, final String name) throws IOException {
 		switch (type) {
 			case NBT.BYTE:
-				return new NBTByte(null, in.readByte());
+				return new NBTByte(name, in.readByte());
 			case NBT.SHORT:
-				return new NBTShort(null, in.readShort());
+				return new NBTShort(name, in.readShort());
 			case NBT.INTEGER:
-				return new NBTInteger(null, in.readInt());
+				return new NBTInteger(name, in.readInt());
 			case NBT.LONG:
-				return new NBTLong(null, in.readLong());
+				return new NBTLong(name, in.readLong());
 			case NBT.FLOAT:
-				return new NBTFloat(null, in.readFloat());
+				return new NBTFloat(name, in.readFloat());
 			case NBT.DOUBLE:
-				return new NBTDouble(null, in.readDouble());
+				return new NBTDouble(name, in.readDouble());
 			case NBT.BYTE_ARRAY:
 				int byteArrayLength = in.readInt();
 				List<Byte> byteArrayBytes = new ArrayList<Byte>(byteArrayLength);
@@ -84,32 +84,32 @@ public class NBTReader {
 				return new NBTByteArray(null, byteArrayBytes);
 			case NBT.STRING:
 				short stringLength = in.readShort();
-				byte[] bytes = new byte[stringLength];
-				in.readFully(bytes);
-				return new NBTString(null, new String(bytes, Charset.forName("UTF-8")));
+				byte[] stringBytes = new byte[stringLength];
+				in.readFully(stringBytes);
+				return new NBTString(name, new String(stringBytes, Charset.forName("UTF-8")));
 			case NBT.LIST:
 				byte listType = in.readByte();
 				int listLength = in.readInt();
 				List<NBT<?>> list = new ArrayList<NBT<?>>(listLength);
 				for (int i = 0; i < listLength; ++i)
-					list.add(this.readPayload(listType));
-				return new NBTList(null, listType, list);
+					list.add(this.readPayload(listType, null));
+				return new NBTList(name, listType, list);
 			case NBT.COMPOUND:
 				NBT<?> tag;
 				List<NBT<?>> tags = new ArrayList<NBT<?>>();
 				while (!((tag = this.readTag()) instanceof NBTEnd))
 					tags.add(tag);
-				return new NBTCompound(null, tags);
+				return new NBTCompound(name, tags);
 			case NBT.INTEGER_ARRAY:
 				int integerArrayLength = in.readInt();
 				List<Integer> integerArrayIntegers = new ArrayList<Integer>(integerArrayLength);
 				for (int i = 0; i < integerArrayLength; i++)
 					integerArrayIntegers.add(in.readInt());
-				return new NBTIntegerArray(null, integerArrayIntegers);
+				return new NBTIntegerArray(name, integerArrayIntegers);
 			default:
 				System.err.println("Unsupported type: " + type);
+				return null;
 		}
-		return null;
 	}
 
 	private NBT<?> readTag() throws IOException {
@@ -121,53 +121,7 @@ public class NBTReader {
 			byte[] bytes = new byte[nameLength];
 			in.readFully(bytes);
 			String name = new String(bytes, Charset.forName("UTF-8"));
-			switch (type) {
-				case NBT.BYTE:
-					return new NBTByte(name, in.readByte());
-				case NBT.SHORT:
-					return new NBTShort(name, in.readShort());
-				case NBT.INTEGER:
-					return new NBTInteger(name, in.readInt());
-				case NBT.LONG:
-					return new NBTLong(name, in.readLong());
-				case NBT.FLOAT:
-					return new NBTFloat(name, in.readFloat());
-				case NBT.DOUBLE:
-					return new NBTDouble(name, in.readDouble());
-				case NBT.BYTE_ARRAY:
-					int byteArrayLength = in.readInt();
-					List<Byte> byteArrayBytes = new ArrayList<Byte>(byteArrayLength);
-					for (int i = 0; i < byteArrayLength; i++)
-						byteArrayBytes.add(in.readByte());
-					return new NBTByteArray(null, byteArrayBytes);
-				case NBT.STRING:
-					short stringLength = in.readShort();
-					byte[] stringBytes = new byte[stringLength];
-					in.readFully(stringBytes);
-					return new NBTString(name, new String(stringBytes, Charset.forName("UTF-8")));
-				case NBT.LIST:
-					byte listType = in.readByte();
-					int listLength = in.readInt();
-					List<NBT<?>> list = new ArrayList<NBT<?>>(listLength);
-					for (int i = 0; i < listLength; ++i)
-						list.add(this.readPayload(listType));
-					return new NBTList(name, listType, list);
-				case NBT.COMPOUND:
-					NBT<?> tag;
-					List<NBT<?>> tags = new ArrayList<NBT<?>>();
-					while (!((tag = this.readTag()) instanceof NBTEnd))
-						tags.add(tag);
-					return new NBTCompound(name, tags);
-				case NBT.INTEGER_ARRAY:
-					int integerArrayLength = in.readInt();
-					List<Integer> integerArrayIntegers = new ArrayList<Integer>(integerArrayLength);
-					for (int i = 0; i < integerArrayLength; i++)
-						integerArrayIntegers.add(in.readInt());
-					return new NBTIntegerArray(name, integerArrayIntegers);
-				default:
-					System.err.println("Unsupported type: " + type);
-			}
+			return this.readPayload(type, name);
 		}
-		return null;
 	}
 }
